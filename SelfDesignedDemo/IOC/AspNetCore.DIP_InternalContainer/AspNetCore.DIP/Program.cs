07299f195c32b71072ff780ba3278e090d5994ed
Panor520 <linpan5065328@foxmail.com>
2021-08-04 17:36:17 +0800

    add new code

--- SelfDesignedDemo/IOC/AspNetCore.DIP_InternalContainer/AspNetCore.DIP/Program.cs
+++ SelfDesignedDemo/IOC/AspNetCore.DIP_InternalContainer/AspNetCore.DIP/Program.cs
@@ -20,17 +20,23 @@ namespace AspNetCore.DIP
                 //这时候发现一个问题①：如果我们要创建的对象中有参数需要传递，而这些参数也是类对象的话就会出现层层依赖，这样就需要自动生成这些参数
                 IPower power1 = SimpleFactory.ServicePowerCreateInstanceArr();
             }
-            //下面来解决上面的问题①
-            {//利用内置IOC容器 把这块的实现放到NormalFactory中
+            #endregion
+
+            #region 利用NormalFactory中的容器解决上面的问题①
+            {//把这块的实现放到NormalFactory中
                 Console.WriteLine("------------------------ASPNETCORE内置容器ServiceCollection-----------------------------");
                 NormalFactory.ServiceCollection();
+                Console.WriteLine("------------------------第三方容器AutoFac-----------------------------");
+                NormalFactory.AutoFac();
+                Console.WriteLine("------------------------第三方容器AutoFac+配置文件（完全脱离细节）-----------------------------");
+                NormalFactory.AutoFacOne();
             }
             #endregion
 
-            #region 利用
-
+            #region 未解决问题
+            //方法注入
+            //生命周期问题
             #endregion
-
             Console.ReadLine();
         }
     }
--- SelfDesignedDemo/IOC/AspNetCore.DIP_InternalContainer/AspNetCore.Factory/NormalFactory.cs
+++ SelfDesignedDemo/IOC/AspNetCore.DIP_InternalContainer/AspNetCore.Factory/NormalFactory.cs
@@ -2,6 +2,9 @@
 using AspNetCore.Interface;
 using AspNetCore.Service;
 using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Autofac.Configuration;
 
 namespace AspNetCore.Factory
 {
@@ -20,28 +23,75 @@ namespace AspNetCore.Factory
         /// AspNetCore内置容器ServiceCollection
         /// 网上说这个方式只适合构造函数注入
         /// 需要Nuget引入：Microsoft.Extensions.DependencyInjection
+        /// 这个方法还是用到了具体类（注册映射时），还没完全脱离细节
         /// </summary>
         public static void ServiceCollection()
         {
-            
+
             IServiceCollection serviceDescriptors = new ServiceCollection();//初始化出来容器
             //2.注册构造对象间的关系(这一步就是在构造抽象类和实现类间的关系)
-            serviceDescriptors.AddTransient(typeof(IPower),typeof(Power));//AddTransient方法中第一个参数是抽象类，第二个参数是实现类
+            serviceDescriptors.AddTransient(typeof(IPower), typeof(Power));//AddTransient方法中第一个参数是抽象类，第二个参数是实现类
             serviceDescriptors.AddTransient(typeof(IMicrophone), typeof(Microphone));
             //3.可以理解为将映射关系生成出来
             ServiceProvider serviceProvider = serviceDescriptors.BuildServiceProvider();//
-            IMicrophone microphone= serviceProvider.GetService<IMicrophone>();
-            IPower power= serviceProvider.GetService<IPower>();//Power构造函数注入
+            IMicrophone microphone = serviceProvider.GetService<IMicrophone>();
+            IPower power = serviceProvider.GetService<IPower>();//Power构造函数注入
         }
 
         /// <summary>
         /// 第三方容器AutoFac
         /// 需要Nuget引入AutoFac
+        /// 这个方法还是用到了具体类（注册映射时），还没完全脱离细节
         /// </summary>
         public static void AutoFac()
         {
-            ContainerBuilder containerBuilder = new ContainerBuilder();
-            containerBuilder.RegisterType<Microphone>().As<IMicrophone>();
+            {//普通构造函数注入
+                System.Console.WriteLine("----------------------------普通构造函数注入");
+                ContainerBuilder containerBuilder = new ContainerBuilder();
+                containerBuilder.RegisterType<Microphone>().As<IMicrophone>();
+                containerBuilder.RegisterType<Power>().As<IPower>();
+                containerBuilder.RegisterType<Applephone>().As<IPhone>();
+                //containerBuilder.RegisterType<Androidphone>().As<IPhone>();
+                containerBuilder.RegisterType<Headphone>().As<IHeadphone>();
+                IContainer container = containerBuilder.Build();
+                IMicrophone microphone = container.Resolve<IMicrophone>();
+                IPhone iphone = container.Resolve<IPhone>();//问题：会按Androidphone构造，而不按Applephone，为什么？
+            }
+
+            {//属性注入 
+                //实现方式，在注册时加上.PropertiesAutowired()
+                System.Console.WriteLine("----------------------普通构造函数注入+属性注入");
+                ContainerBuilder containerBuilder = new ContainerBuilder();
+                containerBuilder.RegisterType<Microphone>().As<IMicrophone>();
+                containerBuilder.RegisterType<Power>().As<IPower>();
+                containerBuilder.RegisterType<Applephone>().As<IPhone>().PropertiesAutowired();//属性注入
+                containerBuilder.RegisterType<Headphone>().As<IHeadphone>();
+                IContainer container = containerBuilder.Build();
+                IMicrophone microphone = container.Resolve<IMicrophone>();
+                IPhone iphone = container.Resolve<IPhone>();
+            }
+        }
+
+        /// <summary>
+        /// 利用配置文件解决抽象注册时依赖细节的问题
+        /// 增加读取配置文件的Nuget：
+        /// </summary>
+        public static void AutoFacOne()
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();//创建IOC容器建造者
+            IConfigurationBuilder configuration = new ConfigurationBuilder();//创建配置文件建造者
+            IConfigurationSource configurationSource = new JsonConfigurationSource()//读出配置文件
+            {
+                Path = "AutoFac.json",
+                Optional=false,//默认是false，可不写
+                ReloadOnChange= true//默认是true，可不写
+            };
+            configuration.Add(configurationSource);//给建造者添加建造工作
+            var module = new ConfigurationModule(configuration.Build());//建造出配置文件映射关系，并生成模型
+            containerBuilder.RegisterModule(module);//IOC容器建造者浏览映射关系模型
+            IContainer container = containerBuilder.Build();//生成出真正的IOC容器
+
+            IMicrophone microphone = container.Resolve<IMicrophone>();//测试生成的IOC容器
         }
     }
 }
